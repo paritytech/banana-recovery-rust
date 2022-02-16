75cd1028cf0bf87b5c9cd5d9f72047a07b9bcbc0
Bernhard Schuster <bernhard@ahoi.io>
2022-02-16 11:05:07 +0100

    use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,64 +1,55 @@
 use crate::shares::{MAX_BITS, MIN_BITS};
 
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[error("Bits in share data {0} are outside of expected range [{min}..={max}]. Likely the share is damaged.", min=MIN_BITS, max=MAX_BITS)]
     BitsOutOfRange(u32),
+    #[error("Decoded secret could not be displayed as a string.")]
     DecodedSecretNotString,
+    #[error("Unable to decode the secret.")]
     DecodingFailed,
+    #[error("Share contains no data.")]
     EmptyShare,
+    #[error("Unable to parse the input as a json object.")]
     JsonParsing,
+    #[error("While processing, tried addressing log[{0}] out of expected range. Likely the share is damaged.")]
     LogOutOfRange(u32),
+    #[error("Nonce is not in base64 format")]
     NonceNotBase64,
+    #[error("ShareSet was not ready to decode. Should not ba here.")]
     NotReadyToDecode,
+    #[error("Received qr code could not be read as a string.")]
     NotShareString,
+    #[error("Unable to parse first data char '{0}' as a number in radix36 format")]
     ParseBit(char),
+    #[error("Required shards value {0} has unsupported format.")]
     RequiredShardsNotSupported(String),
-    ScryptFailed(scrypt::errors::InvalidOutputLen),
+    #[error("Scrypt calculation failed.")]
+    ScryptFailed(#[from] scrypt::errors::InvalidOutputLen),
+    #[error("Share is already in the set.")]
     ShareAlreadyInSet,
+    #[error("Share could not be added to the set, because its bits setting is different.")]
     ShareBitsDifferent,
+    #[error("Share could not be added to the set, because its content length is different.")]
     ShareContentLengthDifferent,
+    #[error("Share could not be added to the set, because its nonce is different.")]
     ShareNonceDifferent,
+    #[error(
+        "Share could not be added to the set, because it has different number of required shards."
+    )]
     ShareRequiredShardsDifferent,
+    #[error("Share could not be added to the set, because its title is different.")]
     ShareTitleDifferent,
+    #[error(
+        "Share content is too short to separate share id properly. Likely the share is damaged."
+    )]
     ShareTooShort,
+    #[error("Share could not be added to the set, because its version is different.")]
     ShareVersionDifferent,
+    #[error("Share with undefined version was expected to have hexadecimal content.")]
     UndefinedBodyNotHex,
+    #[error("Version {.0} is not supported.")]
     VersionNotSupported(String),
+    #[error("Share with version V1 was expected to have content in base64 format.")]
     V1BodyNotBase64,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.show())
-    }
-}
-
-impl Error {
-    pub fn show(&self) -> String {
-        match self {
-            Error::BitsOutOfRange(x) => format!("Bits in share data {} are outside of expected range [{},{}]. Likely the share is damaged.", x, MIN_BITS, MAX_BITS),
-            Error::DecodedSecretNotString => String::from("Decoded secret could not be displayed as a string."),
-            Error::DecodingFailed => String::from("Unable to decode the secret."),
-            Error::EmptyShare => String::from("Share contains no data."),
-            Error::JsonParsing => String::from("Unable to parse the input as a json object."),
-            Error::LogOutOfRange(x) => format!("While processing, tried addressing log[{}] out of expected range. Likely the share is damaged.", x),
-            Error::NonceNotBase64 => String::from("Nonce is not in base64 format"),
-            Error::NotReadyToDecode => String::from("ShareSet was not ready to decode. Should not ba here."),
-            Error::NotShareString => String::from("Received qr code could not be read as a string."),
-            Error::ParseBit(c) => format!("Unable to parse first data char {} as a number in radix36 format", c),
-            Error::RequiredShardsNotSupported(x) => format!("Required shards value {} has unsupported format.", x),
-            Error::ScryptFailed(x) => format!("Scrypt calculation failed. {}", x),
-            Error::ShareAlreadyInSet => String::from("Share is already in the set."),
-            Error::ShareBitsDifferent => String::from("Share could not be added to the set, because its bits setting is different."),
-            Error::ShareContentLengthDifferent => String::from("Share could not be added to the set, because its content length is different."),
-            Error::ShareNonceDifferent => String::from("Share could not be added to the set, because its nonce is different."),
-            Error::ShareRequiredShardsDifferent => String::from("Share could not be added to the set, because it has different number of required shards."),
-            Error::ShareTitleDifferent => String::from("Share could not be added to the set, because its title is different."),
-            Error::ShareTooShort => String::from("Share content is too short to separate share id properly. Likely the share is damaged."),
-            Error::ShareVersionDifferent => String::from("Share could not be added to the set, because its version is different."),
-            Error::UndefinedBodyNotHex => String::from("Share with undefined version was expected to have hexadecimal content."),
-            Error::VersionNotSupported(x) => format!("Version {} is not supported.", x),
-            Error::V1BodyNotBase64 => String::from("Share with version V1 was expected to have content in base64 format."),
-        }
-    }
 }
